570c068bca4ec83186213e1ebd9f824b239a9f23
kimgyver <jinyoung777@gmail.com>
2019-07-08 19:35:38 +1200

    Product Manager Implemented

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManageController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManageController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManageController()
+        {
+            context = new ProductRepository();
+        }
+
+        // GET: ProductManage
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            context.Insert(product);
+            context.Commit();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string id)
+        {
+            Product product = context.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string id)
+        {
+            Product productToEdit = context.Find(id);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            productToEdit.Category = product.Category;
+            productToEdit.Description = product.Description;
+            productToEdit.Image = product.Image;
+            productToEdit.Name = product.Name;
+            productToEdit.Price = product.Price;
+
+            context.Commit();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(string id)
+        {
+            Product product = context.Find(id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string id)
+        {
+            Product product = context.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            context.Delete(id);
+            context.Commit();
+            return RedirectToAction("Index");
+        }
+    }
+}
