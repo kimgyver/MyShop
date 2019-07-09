38ec087e8ec6cf210ab94038b826b0e1a701fd0b
kimgyver <jinyoung777@gmail.com>
2019-07-09 12:03:01 +1200

    unity.mvc applied

--- MyShop/MyShop.WebUI/Controllers/ProductManageController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
@@ -11,13 +12,13 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManageController : Controller
     {
-        InMemoryRepository<Product> context;
-        InMemoryRepository<ProductCategory> productCategory;
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategory;
 
-        public ProductManageController()
+        public ProductManageController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            context = new InMemoryRepository<Product>();
-            productCategory = new InMemoryRepository<ProductCategory>();
+            this.context = productContext;
+            this.productCategory = productCategoryContext;
         }
 
         // GET: ProductManage
